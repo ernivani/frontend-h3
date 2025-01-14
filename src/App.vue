664ec344f1cd785eef99a70ffdd2692580a3979d
Thomas Lindeker <tlindeker0@gmail.com>
2025-01-14 15:39:59 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-gray-100 h-screen w-screen flex justify-center items-center">
+    <h1 class="text-3xl font-bold underline">
+      Hello World
+    </h1>
+  </div>
+</template>
+
